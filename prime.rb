eebf6159f559a1427f21245a575c4809544e0b6c
Arvin Fernandez <arvinf07@gmail.com>
2020-10-24 12:51:35 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+require 'pry'
+
+def prime?(num)
+  divisors = (3..1000000).to_a
+  if num == 2
+    true
+  elsif num > 1 && !(num.even?)  
+    div_array = divisors.select do |divisor|
+      num%divisor == 0
+    end  
+    return true if div_array.size == 1
+    false
+  else 
+    false  
+  end   
+end
+
+
